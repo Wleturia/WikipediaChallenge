0510cd41bb5a6fb856675d00f413637fb9b9d7e5
Walter <leturia@live.com>
2021-10-26 15:29:52 -0500

    Wikipedia Console
    "The app"

---
+++ WikipediaChallenge/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using WikipediaChallenge.Infrastructure.Delivery;
+using WikipediaChallenge.Infrastructure.Repository;
+
+namespace WikipediaChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WikipediaChallenge");
+
+            ApplicationController ac = Initialize();
+
+            ac.GetData();
+        }
+
+        static ApplicationController Initialize()
+        {
+            Repository repository = new();
+            Application.Usecase.Application usecase = new(repository);
+            ApplicationController applicationController = new(usecase);
+
+            return applicationController;
+        }
+    }
+}
