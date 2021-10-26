607022c5af33a9f1643bc5a3bbd476deef9b4f95
Walter <leturia@live.com>
2021-10-26 15:28:10 -0500

    Infrastructure layer
    Delivery Implementation
    - Serve the data
    
    Repository Implementation
    - Retrieve the data

---
+++ WikipediaChallenge.Infrastructure/Delivery/ApplicationController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using WikipediaChallenge.Domain.VO;
+
+namespace WikipediaChallenge.Infrastructure.Delivery
+{
+    public class ApplicationController
+    {
+        readonly Application.Usecase.Application application;
+
+        public ApplicationController(Application.Usecase.Application app)
+        {
+            application = app;
+        }
+
+        public void GetData()
+        {
+            Stack<PageView> pageViews = new Stack<PageView>();
+            PageView p1 = new("it.m", "renault", 10);
+            PageView p2 = new("en", "apple", 50);
+            pageViews.Push(p1);
+            pageViews.Push(p2);
+
+
+            application.GetPageViewForPreviuosHours(5);
+
+            ConsoleTables.ConsoleTable.From<PageView>(pageViews).Write();
+            return;
+        }
+    }
+}
---
+++ WikipediaChallenge.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WikipediaChallenge.Domain.Repository;
+
+namespace WikipediaChallenge.Infrastructure.Repository
+{
+    public class Repository : IRepository
+    {
+        public void Test()
+        {
+            Console.WriteLine("Repository");
+        }
+    }
+}
