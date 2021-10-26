680680e31a179c5a64f791f517cec7d3e23ca49c
Walter <leturia@live.com>
2021-10-26 15:29:09 -0500

    Application Layer
    Usecases implementation
    
    It's connected to repository

---
+++ WikipediaChallenge.Application/Usecase/Application.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using WikipediaChallenge.Domain.Entity;
+using WikipediaChallenge.Domain.Repository;
+using WikipediaChallenge.Domain.Usecase;
+
+namespace WikipediaChallenge.Application.Usecase
+{
+    public class Application : IApplication
+    {
+        private readonly IRepository repository;
+
+        public Application(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IEnumerable<PageView> GetPageViewForPreviuosHours(int hours)
+        {
+            repository.Test();
+            Console.WriteLine("Consultando data" + hours);
+
+            return null;
+        }
+    }
+}
