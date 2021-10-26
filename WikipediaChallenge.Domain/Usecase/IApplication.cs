e5cb48469d03930b9664f8ecdd7698fca3f373d0
Walter <leturia@live.com>
2021-10-26 15:26:46 -0500

    Domain layer. Entity *Classes Repository *Interfaces Usecase *Interfaces VO *Classes

---
+++ WikipediaChallenge.Domain/Usecase/IApplication.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace WikipediaChallenge.Domain.Usecase
+{
+    public interface IApplication
+    {
+        public IEnumerable<Entity.PageView> GetPageViewForPreviuosHours(int hours);
+    }
+}
---
+++ WikipediaChallenge.Domain/VO/PageView.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WikipediaChallenge.Domain.VO
+{
+    public class PageView
+    {
+        public PageView(string domainCode, string pageTitle, int cnt)
+        {
+            DomainCode = domainCode;
+            PageTitle = pageTitle;
+            CNT = cnt;
+        }
+
+        public string DomainCode { get; set; }
+        public string PageTitle { get; set; }
+        public int CNT { get; set; }
+    }
+}
