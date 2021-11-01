5167985fba56945ef2b297a746530ab1e0a2aafe
Walter <leturia@live.com>
2021-10-31 21:27:47 -0500

    PageView Entity, props and constructor

--- WikipediaChallenge.Domain/Entity/PageView.cs
+++ WikipediaChallenge.Domain/Entity/PageView.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WikipediaChallenge.Domain.Entity
+﻿namespace WikipediaChallenge.Domain.Entity
 {
     public class PageView
     {
+        public PageView(string domainCode, string pageTitle, int countViews, string totalResponseSize)
+        {
+            this.domainCode = domainCode;
+            this.pageTitle = pageTitle;
+            this.countViews = countViews;
+            this.totalResponseSize = totalResponseSize;
+        }
+        public string domainCode { get; set; }
+        public string pageTitle { get; set; }
+        public int countViews { get; set; }
+        public string totalResponseSize { get; set; }
     }
 }
